1f2bfd9d9e99e6ac4836c52893ae0d81e9660276
chris-larham-1983 <chrislarham@outlook.com>
2023-03-11 11:53:10 +0000

    Final PERN stack App public commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,10 @@
 //React configuration
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-//<Audio /> component
-import Audio from './components/Audio/Audio';
 //pages
 import IntroPage from './pages/IntroPage';
 import SolSelectPage from './pages/SolSelectPage';
 import SolPage from './pages/SolPage';
-import FillManifestTable from './pages/FillManifestTable';
-import FillPhotosTable from "./pages/FillPhotosTable";
-import AddFirstPhotoDetailsToManifestTable from './pages/AddFirstPhotoDetailsToManifestTable';
 //styling
 import styles from './styling/styling.module.css';
 import './App.css';
@@ -24,9 +19,6 @@ function App() {
                     <Route exact path="/intro" element={<IntroPage />} />
                     <Route exact path="/solSelect" element={<SolSelectPage />} />
                     <Route exact path="/sols/:sol" element={<SolPage />} />
-                    <Route exact path="/fillManifest" element={<FillManifestTable />} />
-                    <Route exact path="/fillPhotos" element={<FillPhotosTable />} />
-                    <Route exact path="/addToManifest" element={<AddFirstPhotoDetailsToManifestTable />} />
                     <Route path="/*" element={<Navigate to="/intro" />} />
                 </Routes>
             </Router>
